16f009894e52a913543ceaceeef65e1a19c510f0
tchiro95 <tchiro95@gmail.com>
2024-02-02 21:15:16 +0900

    namedSlot

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,8 @@
 <x-tests.app>
+  <x-slot name="header">
+    ヘッドロココ<br>
+    <div class=" border-b-2 text-red-400 font-bold">やまちちのぽ</div>
+
+  </x-slot>
   コンポーネントテスト１
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,9 @@
 <x-tests.app>
+  <x-slot name="header">
+    ワンダー<br>
+    <div class=" border-b-2 text-red-400 font-bold">やまちちのぽ</div>
+
+  </x-slot>
+
   コンポーネントテスト2
 </x-tests.app>
