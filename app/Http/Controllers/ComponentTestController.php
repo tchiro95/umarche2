f84c26e0d0b7569b2f30382da2cc7249abcba841
tchiro95 <tchiro95@gmail.com>
2024-02-02 23:03:03 +0900

    compact

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -10,7 +10,9 @@ class ComponentTestController extends Controller
 
     public function showComponent1()
     {
-        return view('tests.component-test1');
+        $message = 'メッセージ';
+        return view('tests.component-test1', compact("message"));
+        //compactは連想配列を作る。keyが変数名と同じになるので、compact('keyName')となって、$keyNameとはならないことに注意
     }
     public function showComponent2()
     {
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,5 @@
     <div class=" border-b-2 text-red-400 font-bold">やまちちのぽ</div>
   </x-slot>
   コンポーネントテスト１
-  <x-tests.card title="タイトル" content="本文"></x-tests.card>
+  <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
 </x-tests.app>
