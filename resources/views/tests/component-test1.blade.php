c13d456048ebe26af30d6d07dd247cf0def52012
tchiro95 <tchiro95@gmail.com>
2024-02-02 21:45:47 +0900

    attribute

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,7 +2,7 @@
   <x-slot name="header">
     ヘッドロココ<br>
     <div class=" border-b-2 text-red-400 font-bold">やまちちのぽ</div>
-
   </x-slot>
   コンポーネントテスト１
+  <x-tests.card title="タイトル" content="本文"></x-tests.card>
 </x-tests.app>
