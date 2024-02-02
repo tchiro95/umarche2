62e82efa14a5960d7e6bd3f954c1e28a0be36ef2
tchiro95 <tchiro95@gmail.com>
2024-02-02 20:56:52 +0900

    componentTest

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ComponentTestController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('component-test1', [ComponentTestController::class, 'showComponent1']);
+
+Route::get('component-test2', [ComponentTestController::class, 'showComponent2']);
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -28,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
