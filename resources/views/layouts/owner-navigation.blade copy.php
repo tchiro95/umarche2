09bd533dc68d7097c7ec7febf18ae7b6895a9b6c
tchiro95 <tchiro95@gmail.com>
2024-02-06 15:32:41 +0900

    リソースコントローラー

--- resources/views/layouts/owner-navigation.blade copy.php
+++ resources/views/layouts/owner-navigation.blade copy.php
@@ -6,7 +6,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('owner.dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <div class="w-28">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        </div>
                     </a>
                 </div>
 
--- routes/admin.php
+++ routes/admin.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,7 @@ Route::get('/', function () {
     return view('admin.welcome');
 });
 
+Route::resource('owners', OwnersController::class)->middleware('auth:admin');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
